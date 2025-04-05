858f532d3a6a05999fbd3d9bc9fed4d8a79791c4
Charlie Wallin <charliekwallin@gmail.com>
2025-04-05 09:59:37 -0400

    Clenaing up files in prepartion for video.

---
+++ phase04/private/db_credentials.php
@@ -0,0 +1,12 @@
+<?php
+// local
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "sally");
+// define("DB_PASS", "P@ssword1234");
+// define("DB_NAME", "salamanders");
+
+// SiteGround
+define("DB_SERVER", "localhost");
+define("DB_USER", "ulpmyxg7g6b7a");
+define("DB_PASS", "P@ssword1234-4321");
+define("DB_NAME", "dbnywkxx35sncd");
