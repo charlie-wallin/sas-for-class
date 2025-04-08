6e73e2cf43604d41b2c78a66ca006f4c82269914
Charlie Wallin <charliekwallin@gmail.com>
2025-04-08 11:27:01 -0400

    Created a .env file and put in .gitignore

--- phase06/private/db_credentials.php
+++ phase06/private/db_credentials.php
@@ -6,7 +6,12 @@
 // define("DB_NAME", "salamanders");
 
 // SiteGround
-define("DB_SERVER", "localhost");
-define("DB_USER", "ulpmyxg7g6b7a");
-define("DB_PASS", "P@ssword1234-4321");
-define("DB_NAME", "dbnywkxx35sncd");
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "ulpmyxg7g6b7a");
+// define("DB_PASS", "P@ssword1234-4321");
+// define("DB_NAME", "dbnywkxx35sncd");
+
+define("DB_SERVER", $_ENV['DB_SERVER']);
+define("DB_USER", $_ENV['DB_USER']);
+define("DB_PASS", $_ENV['DB_PASS']);
+define("DB_NAME", $_ENV['DB_NAME']);
